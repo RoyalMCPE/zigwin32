d42defa1912833e19e9134a0feec642f8ab8a813
RoyalMCPE <coolestgamer117@gmail.com>
2024-03-13 22:19:26 -0600

    .

--- build.zig
+++ build.zig
@@ -4,12 +4,7 @@ const std = @import("std");
 const PACKAGE_NAME = "zigwin32";
 
 pub fn build(b: *std.Build) void {
-    const optimize = b.standardOptimizeOption(.{});
-    const target = b.standardTargetOptions(.{});
-
     _ = b.addModule(PACKAGE_NAME, .{
         .source_file = .{ .path = "win32.zig" },
-        .optimize = optimize,
-        .target = target,
     });
 }
