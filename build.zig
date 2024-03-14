0c7f0ea89b7807256f91e94ea53bd3aa2d5c4b1a
RoyalMCPE <coolestgamer117@gmail.com>
2024-03-13 22:16:43 -0600

    Update build.zig

--- build.zig
+++ build.zig
@@ -1,16 +1,15 @@
 const builtin = @import("builtin");
 const std = @import("std");
 
-const is_zig_0_11 = std.mem.eql(u8, builtin.zig_version_string, "0.11.0");
+const PACKAGE_NAME = "zigwin32";
 
 pub fn build(b: *std.Build) void {
-    if (is_zig_0_11) {
-        _ = b.addModule("zigwin32", .{
-            .source_file = .{ .path = "win32.zig" },
-        });
-    } else {
-        _ = b.addModule("zigwin32", .{
-            .root_source_file = .{ .path = "win32.zig" },
-        });
-    }
+    const optimize = b.standardOptimizeOption(.{});
+    const target = b.standardTargetOptions(.{});
+
+    _ = b.addModule(PACKAGE_NAME, .{
+        .source_file = .{ .path = "win32.zig" },
+        .optimize = optimize,
+        .target = target,
+    });
 }
